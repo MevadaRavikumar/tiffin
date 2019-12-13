5ac85c8a5f0939ef8bd1a6931298a2bbb2858e4a
MevadaRavikumar <mevada.ravikumar@gmail.com>
2019-12-13 22:22:15 +0100

    front end got updated with static

--- MenuPage/urls.py
+++ MenuPage/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('menu_categories/', views.menu_categories, name='menu_categories'),
+    path('single_categories/', views.single_categories, name='single_categories'),
+
 ]
--- MenuPage/views.py
+++ MenuPage/views.py
@@ -5,3 +5,9 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+def menu_categories(request):
+    return render (request, 'menu-categories.html')
+
+def single_categories(request):
+    return render (request, 'single-categories.html')
